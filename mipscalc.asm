586f5fa41856a651fafd3fbb9ba818d172dbe956
DazedFury <jmtexas19@gmail.com>
2018-09-23 15:39:39 -0500

    style: Add newline after operator input

--- mipscalc.asm
+++ mipscalc.asm
@@ -15,7 +15,7 @@
 .text
 #MAIN
 main:
-	#GETINPUT
+	#GETINPUT1
 	la		$a0, inputString1		#Load pointer inputString1 into $a0
 	la		$a1, input1			#Load pointer input1 into $a1
 	jal 		getInput			#Jump to procedure printInputStr1
@@ -24,6 +24,16 @@ main:
 	la		$a0, inputOperatorStr		#Load pointer inputOperatorStr into $a0
 	la		$a1, inputOperatorStr		#Load pointer operator into $a1
 	jal		getOperator
+	
+	#FORMATTING NEWLINE
+	li		$v0, 11				#Load print character syscall
+	addi		$a0, $0, 0xA			#Load ascii character for newline into $a0
+	syscall						#Execute
+	
+	#GETINPUT2
+	la		$a0, inputString2		#Load pointer inputString1 into $a0
+	la		$a1, input2			#Load pointer input1 into $a1
+	jal 		getInput			#Jump to procedure printInputStr1
 	
 	#EXIT
 	li		$v0, 17				#Load exit syscall
